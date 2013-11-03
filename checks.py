87579f07a876289ffae485d402d65ed77ddb7769
Mark Breedlove <mb@markbreedlove.com>
2013-11-03 09:28:32 -0500

    Added ForbiddenError to ourexceptions and changed checks functions to return more specific exceptions.

--- checks.py
+++ checks.py
@@ -1,18 +1,20 @@
 
 __all__ = ['check_float', 'check_int', 'check_notempty']
 
+from ourexceptions import BadRequestError
+
 def check_float(n):
     if not type(n) == float:
-        raise Exception("%s is not float" % n)
+        raise BadRequestError("%s is not float" % n)
     return n
 
 def check_int(n):
     if not type(n) == int:
-        raise Exception("%s is not int" % n)
+        raise BadRequestError("%s is not int" % n)
     return n
 
 def check_notempty(s):
     if len(s) == 0:
-        raise Exception("got empty value")
+        raise BadRequestError("got empty value")
     return s
 
--- ourexceptions.py
+++ ourexceptions.py
@@ -1,8 +1,11 @@
-__all__ = ['BadRequestError', 'ConflictError']
+__all__ = ['BadRequestError', 'ConflictError', 'ForbiddenError']
 
 class BadRequestError(Exception):
     pass
 
 class ConflictError(Exception):
+    pass
+
+class ForbiddenError(Exception):
     pass
 
